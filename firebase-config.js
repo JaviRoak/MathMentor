6cb4b28823dd8719e9dce32e9f22964d1c7eae21
JaviRoak <161542044+JaviRoak@users.noreply.github.com>
2026-05-20 18:08:06 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Configuracion publica de Firebase para iniciar sesion con Google.
+window.MathMentorFirebaseConfig = {
+  apiKey: 'AIzaSyAkSUvYmM4i07krZVn_WbQY7F9pjMg3ALU',
+  authDomain: 'mathmentor-3fd62.firebaseapp.com',
+  projectId: 'mathmentor-3fd62',
+  storageBucket: 'mathmentor-3fd62.firebasestorage.app',
+  messagingSenderId: '9563947136',
+  appId: '1:9563947136:web:1103e1ac8a00a43f37a665',
+  measurementId: 'G-ENQDYF5J84',
+};
